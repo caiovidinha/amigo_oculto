9d5817104d9712b74cbdb588ca29ecfa7251a5a4
Caio Vidinha <caiovidinha@id.uff.br>
2023-12-19 18:02:57 -0300

    base backend

---
+++ src/utils/requestIntercepter.ts
@@ -0,0 +1,9 @@
+import { RequestHandler } from "express";
+import router from "../routes/site";
+
+export const requestIntercepter: RequestHandler = (req, res, next) => {
+    console.log(`-> ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`)
+    next()
+}
+
+export default router
