b841f19320260021c460faf232ba52418c7ca364
Caio Vidinha <caiovidinha@id.uff.br>
2023-12-20 14:24:15 -0300

    CRUDs finalizados e lógica do sorteio

--- src/utils/requestIntercepter.ts
+++ src/utils/requestIntercepter.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from "express";
 import router from "../routes/site";
 
 export const requestIntercepter: RequestHandler = (req, res, next) => {
-    console.log(`-> ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`)
+    console.log(`➡️ ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`)
     next()
 }
 
